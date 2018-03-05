37f3eed7b67b50023fa847acbd912e7b87a7f79e
Steve Palmer <steve@srpalmer.me.uk>
2018-03-05 15:04:28 +0000

    brought up-to-date

--- test/test_modulo_n.py
+++ test/test_modulo_n.py
@@ -14,28 +14,51 @@ from src import *
 from modulo_n import ModuloN
 
 
-# @Given(st.builds(ModuloN.bit, st.integers()))
-# class Test_ModuloN_bit(EqualityTests, FieldTests, LatticeWithComplement):
-#     zero = ModuloN.bit(0)
-#     one = ModuloN.bit(1)
-#     bottom = zero
-#     top = one
+class ModuloNTests(EqualityTests, FieldTests, LatticeWithComplement):
+
+    @property
+    def bottom(self):
+        return self.zero
+
+    @property
+    def top(self):
+        return -self.one
+
+    unittest.skip("bottom is zero")
+    def test_generic_2000_bottom_type(self):
+        pass
+
+    unittest.skip("top is -one")
+    def test_generic_2001_top_type(self):
+        pass
+
+    def test_generic_2020_zero_type(self):
+        self.assertIsInstance(self.zero, ModuloN)
+
+    def test_generic_2021_one_type(self):
+        self.assertIsInstance(self.one, ModuloN)
 
 
-# @Given(st.builds(ModuloN.byte, st.integers()))
-# class Test_ModuloN_byte(EqualityTests, FieldTests, LatticeWithComplement):
-#     zero = ModuloN.byte(0)
-#     one = ModuloN.byte(1)
-#     bottom = zero
-#     top = ModuloN.byte(-1)
+@Given(st.builds(ModuloN.bit, st.integers()))
+class Test_ModuloN_bit(ModuloNTests):
+    zero = ModuloN.bit(0)
+    one = ModuloN.bit(1)
+    bottom = zero
+    top = one
+
+
+@Given(st.builds(ModuloN.byte, st.integers()))
+class Test_ModuloN_byte(ModuloNTests):
+    zero = ModuloN.byte(0)
+    one = ModuloN.byte(1)
+    bottom = zero
+    top = ModuloN.byte(-1)
 
 
 @Given(st.builds(ModuloN.word, st.integers()))
-class Test_ModuloN_word(EqualityTests, FieldTests, FloorDivModTests, LatticeWithComplement):
+class Test_ModuloN_word(ModuloNTests):
     zero = ModuloN.word(0)
     one = ModuloN.word(1)
-    bottom = zero
-    top = ModuloN.word(-1)
 
 
 if __name__ == '__main__':
