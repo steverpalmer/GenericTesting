2f0a855919797857f58f56c54114e8768977a263
Steve Palmer <steve@srpalmer.me.uk>
2018-03-07 04:39:06 +0000

    Corrected my Python syntax.

--- src/augmented_assignment.py
+++ src/augmented_assignment.py
@@ -16,13 +16,13 @@ from .core import GenericTests, ClassUnderTest
 class LatticeAugmentedAssignmentTests(GenericTests):
 
     def test_generic_2280_ior_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a |= b: a == a₀ | b"
+        "a |= b; a == a₀ | b"
         a_expected = a | b
         a |= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2281_iand_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a &= b: a == a₀ & b"
+        "a &= b; a == a₀ & b"
         a_expected = a & b
         a &= b
         self.assertEqual(a, a_expected)
@@ -31,13 +31,13 @@ class LatticeAugmentedAssignmentTests(GenericTests):
 class LatticeWithComplementAugmentedTests(LatticeAugmentedAssignmentTests):
 
     def test_generic_2282_isub_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a -= b: a == a₀ - b"
+        "a -= b; a == a₀ - b"
         a_expected = a - b
         a -= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2283_ixor_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a ^= b: a == a₀ ^ b"
+        "a ^= b; a == a₀ ^ b"
         a_expected = a ^ b
         a ^= b
         self.assertEqual(a, a_expected)
@@ -46,25 +46,25 @@ class LatticeWithComplementAugmentedTests(LatticeAugmentedAssignmentTests):
 class ComplexAugmentedAssignmentTests(GenericTests):
 
     def test_generic_2282_isub_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a -= b: a == a₀ - b"
+        "a -= b; a == a₀ - b"
         a_expected = a - b
         a -= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2284_iadd_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a += b: a == a₀ + b"
+        "a += b; a == a₀ + b"
         a_expected = a + b
         a += b
         self.assertEqual(a, a_expected)
 
     def test_generic_2285_imul_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a *= b: a == a₀ * b"
+        "a *= b; a == a₀ * b"
         a_expected = a * b
         a *= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2286_itruediv_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a /= b: a == a₀ / b"
+        "a /= b; a == a₀ / b"
         assume(not b == self.zero)
         a_expected = a / b
         a /= b
@@ -74,14 +74,14 @@ class ComplexAugmentedAssignmentTests(GenericTests):
 class FloorDivAugmentedAssignmentTests(GenericTests):
 
     def test_generic_2287_ifloordiv_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a //= b: a == a₀ // b"
+        "a //= b; a == a₀ // b"
         assume(not b == self.zero)
         a_expected = a // b
         a //= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2288_imod_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "a %= b: a == a₀ % b"
+        "a %= b; a == a₀ % b"
         assume(not b == self.zero)
         a_expected = a % b
         a %= b
@@ -91,14 +91,14 @@ class FloorDivAugmentedAssignmentTests(GenericTests):
 class IntegralAugmentedAssignmentTests(ComplexAugmentedAssignmentTests):
 
     def test_generic_2392_ilshift_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "0 <= b ⇒ a <<= b: a == a₀ << b"
+        "0 <= b ⇒ a <<= b; a == a₀ << b"
         assume(self.zero <= b)
         a_expected = a << b
         a <<= b
         self.assertEqual(a, a_expected)
 
     def test_generic_2393_irshift_definition(self, a: ClassUnderTest, b: ClassUnderTest):
-        "0 <= b ⇒ a >>= b: a == a₀ >> b"
+        "0 <= b ⇒ a >>= b; a == a₀ >> b"
         assume(self.zero <= b)
         a_expected = a >> b
         a >>= b
