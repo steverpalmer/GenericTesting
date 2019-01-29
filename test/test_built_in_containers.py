cb687b2c0f44f6dfdf64efa6bdcf32dd0eebc157
Steve Palmer <steve@srpalmer.me.uk>
2019-01-29 08:13:44 +0000

    removed redundant duplicate testing

--- test/test_built_in_containers.py
+++ test/test_built_in_containers.py
@@ -169,11 +169,11 @@ __all__ = ('Test_frozenset', 'Test_set',
 
 if __name__ == '__main__':
     SUITE = unittest.TestSuite()
-    name = None  # :TRICK: need to introdcue 'name' before iterating through locals
-    value = None  # :TRICK: need to introdcue 'value' before iterating through locals
-    for name, value in locals().items():
-        if name.startswith('Test_'):
-            SUITE.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(value))
+    #  name = None  # :TRICK: need to introdcue 'name' before iterating through locals
+    #  value = None  # :TRICK: need to introdcue 'value' before iterating through locals
+    #  for name, value in locals().items():
+    #      if name.startswith('Test_'):
+    #          SUITE.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(value))
     SUITE.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(Test_frozenset))
     SUITE.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(Test_set))
     SUITE.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(Test_dict_KeysView))
