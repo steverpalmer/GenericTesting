8bc4345d7ccd39cdbb5c7a7eab2462601bbbec49
Steve Palmer <steve@srpalmer.me.uk>
2019-02-06 07:36:22 +0000

    WIP

--- test/read.py
+++ test/read.py
@@ -3,13 +3,14 @@ import os
 import io
 import time
 
-stdin_raw = sys.stdin.buffer.raw
-os.set_blocking(stdin_raw.fileno(), False)
-stdin_buffer = io.BufferedReader(stdin_raw)
+#stdin_raw = sys.stdin.buffer.raw
+#os.set_blocking(stdin_raw.fileno(), False)
+stdin_buffer = sys.stdin.buffer
 print(repr(stdin_buffer))
+print(stdin_buffer.isatty())
 for _ in range(20):
     try:
-        print(repr(stdin_buffer.read()))
+        print(repr(stdin_buffer.read(2)))
     except Exception as exc:
         print(repr(exc))
     time.sleep(0.1)
